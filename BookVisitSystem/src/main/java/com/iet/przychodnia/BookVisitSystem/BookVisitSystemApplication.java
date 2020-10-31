046489d97279d3ff8ebac63ebc088708fdd20eae
Robert Cialowicz <robertcialowicz@gmail.com>
2020-10-31 21:43:03 +0100

    Init Commit
    Project BookVisit initialized

---
+++ BookVisitSystem/src/main/java/com/iet/przychodnia/BookVisitSystem/BookVisitSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iet.przychodnia.BookVisitSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookVisitSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookVisitSystemApplication.class, args);
+	}
+
+}
---
+++ BookVisitSystem/src/test/java/com/iet/przychodnia/BookVisitSystem/BookVisitSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iet.przychodnia.BookVisitSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookVisitSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

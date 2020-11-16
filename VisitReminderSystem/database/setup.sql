ea1597ba2572843c36f2baf4ddfb6ad55542b5af
Robert Cialowicz <robertcialowicz@gmail.com>
2020-11-16 22:59:36 +0100

    Each subsystem has setup database as dockerfile

---
+++ VisitReminderSystem/database/setup.sql
@@ -0,0 +1,104 @@
+USE [master]
+GO
+/****** Object:  Database [dbReminders]    Script Date: 16.11.2020 22:42:37 ******/
+CREATE DATABASE [dbReminders]
+    CONTAINMENT = NONE
+    ON  PRIMARY
+    ( NAME = N'dbReminders', FILENAME = N'/var/opt/mssql/data/dbReminders.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+    LOG ON
+    ( NAME = N'dbReminders_log', FILENAME = N'/var/opt/mssql/data/dbReminders_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+    WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [dbReminders] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+    begin
+        EXEC [dbReminders].[dbo].[sp_fulltext_database] @action = 'enable'
+    end
+GO
+ALTER DATABASE [dbReminders] SET ANSI_NULL_DEFAULT OFF
+GO
+ALTER DATABASE [dbReminders] SET ANSI_NULLS OFF
+GO
+ALTER DATABASE [dbReminders] SET ANSI_PADDING OFF
+GO
+ALTER DATABASE [dbReminders] SET ANSI_WARNINGS OFF
+GO
+ALTER DATABASE [dbReminders] SET ARITHABORT OFF
+GO
+ALTER DATABASE [dbReminders] SET AUTO_CLOSE OFF
+GO
+ALTER DATABASE [dbReminders] SET AUTO_SHRINK OFF
+GO
+ALTER DATABASE [dbReminders] SET AUTO_UPDATE_STATISTICS ON
+GO
+ALTER DATABASE [dbReminders] SET CURSOR_CLOSE_ON_COMMIT OFF
+GO
+ALTER DATABASE [dbReminders] SET CURSOR_DEFAULT  GLOBAL
+GO
+ALTER DATABASE [dbReminders] SET CONCAT_NULL_YIELDS_NULL OFF
+GO
+ALTER DATABASE [dbReminders] SET NUMERIC_ROUNDABORT OFF
+GO
+ALTER DATABASE [dbReminders] SET QUOTED_IDENTIFIER OFF
+GO
+ALTER DATABASE [dbReminders] SET RECURSIVE_TRIGGERS OFF
+GO
+ALTER DATABASE [dbReminders] SET  DISABLE_BROKER
+GO
+ALTER DATABASE [dbReminders] SET AUTO_UPDATE_STATISTICS_ASYNC OFF
+GO
+ALTER DATABASE [dbReminders] SET DATE_CORRELATION_OPTIMIZATION OFF
+GO
+ALTER DATABASE [dbReminders] SET TRUSTWORTHY OFF
+GO
+ALTER DATABASE [dbReminders] SET ALLOW_SNAPSHOT_ISOLATION OFF
+GO
+ALTER DATABASE [dbReminders] SET PARAMETERIZATION SIMPLE
+GO
+ALTER DATABASE [dbReminders] SET READ_COMMITTED_SNAPSHOT OFF
+GO
+ALTER DATABASE [dbReminders] SET HONOR_BROKER_PRIORITY OFF
+GO
+ALTER DATABASE [dbReminders] SET RECOVERY FULL
+GO
+ALTER DATABASE [dbReminders] SET  MULTI_USER
+GO
+ALTER DATABASE [dbReminders] SET PAGE_VERIFY CHECKSUM
+GO
+ALTER DATABASE [dbReminders] SET DB_CHAINING OFF
+GO
+ALTER DATABASE [dbReminders] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF )
+GO
+ALTER DATABASE [dbReminders] SET TARGET_RECOVERY_TIME = 60 SECONDS
+GO
+ALTER DATABASE [dbReminders] SET DELAYED_DURABILITY = DISABLED
+GO
+ALTER DATABASE [dbReminders] SET ACCELERATED_DATABASE_RECOVERY = OFF
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'dbReminders', N'ON'
+GO
+ALTER DATABASE [dbReminders] SET QUERY_STORE = OFF
+GO
+USE [dbReminders]
+GO
+/****** Object:  Table [dbo].[Reminders]    Script Date: 16.11.2020 22:42:37 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Reminders](
+                                  [id] [varchar](50) NOT NULL,
+                                  [patientID] [varchar](50) NOT NULL,
+                                  [doctorID] [varchar](50) NOT NULL,
+                                  [date] [datetime] NOT NULL,
+                                  CONSTRAINT [PK_Reminders] PRIMARY KEY CLUSTERED
+                                      (
+                                       [id] ASC
+                                          )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [dbReminders] SET  READ_WRITE
+GO

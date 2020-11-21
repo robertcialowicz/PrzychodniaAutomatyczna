10a2ff4b13d9b77b73a00aabb3e9092f7789bf47
Robert Cialowicz <robertcialowicz@gmail.com>
2020-11-21 17:10:36 +0100

    acsfafsafa

--- user-authentication-system/src/test/java/com/iet/przychodnia/user_authentication_system/UserAuthenticationSystemApplicationTests.java
+++ user-authentication-system/src/test/java/com/iet/przychodnia/user_authentication_system/UserAuthenticationSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserAuthenticationSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }

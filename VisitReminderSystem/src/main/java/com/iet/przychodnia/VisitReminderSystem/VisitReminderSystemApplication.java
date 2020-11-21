de8ad30463f626ee2a01a8d452ede78253252cce
Robert Cialowicz <robertcialowicz@gmail.com>
2020-11-21 17:38:37 +0100

    VisitReminderSystem added to eureka

--- VisitReminderSystem/src/main/java/com/iet/przychodnia/VisitReminderSystem/VisitReminderSystemApplication.java
+++ VisitReminderSystem/src/main/java/com/iet/przychodnia/VisitReminderSystem/VisitReminderSystemApplication.java
@@ -2,8 +2,10 @@ package com.iet.przychodnia.VisitReminderSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VisitReminderSystemApplication {
 
 	public static void main(String[] args) {

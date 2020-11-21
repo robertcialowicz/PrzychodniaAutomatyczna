db9f35253d3e526843666438d738aac58b156045
Robert Cialowicz <robertcialowicz@gmail.com>
2020-11-21 17:09:35 +0100

    BookVisitSystem added  to eureka

--- BookVisitSystem/src/main/java/com/iet/przychodnia/BookVisitSystem/BookVisitSystemApplication.java
+++ BookVisitSystem/src/main/java/com/iet/przychodnia/BookVisitSystem/BookVisitSystemApplication.java
@@ -3,9 +3,11 @@ package com.iet.przychodnia.BookVisitSystem;
 import com.iet.przychodnia.BookVisitSystem.controller.VisitController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookVisitSystemApplication {
 
 	public static void main(String[] args) {
